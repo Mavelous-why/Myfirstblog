6c20235c1fa00be80fa5d259764a9e3937bb974f
Avril <avril.gray@phbillies.org>
2018-01-19 14:16:50 -0800

    hello

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,4 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {'posts':posts})
